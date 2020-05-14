42a164feb8782c8d1b3e49e7a22227635c336fec
TakaneP <40864488+TakaneP@users.noreply.github.com>
2020-05-15 04:32:36 +0800

    Update data_structure.cc

--- sw_cc/data_structure.cc
+++ sw_cc/data_structure.cc
@@ -39,10 +39,70 @@ void RoutingGraph::add_cell_demand_into_graph(int x, int y, int MCtype) {
 	}
 	//Update current
         for(auto _it = it->second.begin(); _it != it->second.end(); ++_it) {
+	    if((preAfterPairCnt - preOriginalPairCnt + nxtAfterPairCnt - nxtOriginalPairCnt)>0) cout << x << y <<endl;
             grids[x][y][_it->first].demand += _it->second * (preAfterPairCnt - preOriginalPairCnt + nxtAfterPairCnt - nxtOriginalPairCnt);
 	}
     }
     cellCount[x][y][MCtype]++;
+}
+
+void RoutingGraph::del_cell_demand_from_graph(int x, int y, int MCtype) {
+    //Delete blockage demand
+    MasterCell& masterCell = masterCells[MCtype];
+    for(auto it = masterCell.blockages.begin(); it != masterCell.blockages.end(); ++it) {
+        grids[x][y][it->first].demand -= it->second;
+    }
+    //Delete sameGGrid rule
+    for(auto it = sameGGrid[MCtype].begin(); it != sameGGrid[MCtype].end(); ++it) {
+        int MCtype2 = it->first;
+        int originalPairCnt = min(cellCount[x][y][MCtype], cellCount[x][y][MCtype2]);
+        int afterPairCnt = min(cellCount[x][y][MCtype] - 1, cellCount[x][y][MCtype2]);
+        for(auto _it = it->second.begin(); _it != it->second.end(); ++_it) {
+            grids[x][y][_it->first].demand -= _it->second * (originalPairCnt - afterPairCnt);
+        }
+    }
+    //Delete adjHGGrid rule
+    for(auto it = adjHGGrid[MCtype].begin(); it != adjHGGrid[MCtype].end(); ++it) {
+        int MCtype2 = it->first, preOriginalPairCnt = 0, preAfterPairCnt = 0, nxtOriginalPairCnt = 0, nxtAfterPairCnt = 0;
+        //Delete left
+        if(y > 0) {
+            preOriginalPairCnt = min(cellCount[x][y-1][MCtype2], cellCount[x][y][MCtype]);
+            preAfterPairCnt = min(cellCount[x][y-1][MCtype2], cellCount[x][y][MCtype] - 1);
+            for(auto _it = it->second.begin(); _it != it->second.end(); ++_it) {
+                grids[x][y-1][_it->first].demand -= _it->second * (preOriginalPairCnt - preAfterPairCnt);
+            }
+        }
+        //Delete right
+        if(y < (column - 1)) {
+            nxtOriginalPairCnt = min(cellCount[x][y+1][MCtype2], cellCount[x][y][MCtype]);
+            nxtAfterPairCnt = min(cellCount[x][y+1][MCtype2], cellCount[x][y][MCtype] - 1);
+            for(auto _it = it->second.begin(); _it != it->second.end(); ++_it) {
+                grids[x][y+1][_it->first].demand -= _it->second * (nxtOriginalPairCnt - nxtAfterPairCnt);
+            }
+        }
+        //Delete current
+        for(auto _it = it->second.begin(); _it != it->second.end(); ++_it) {
+            grids[x][y][_it->first].demand -= _it->second * (preOriginalPairCnt  - preAfterPairCnt + nxtOriginalPairCnt - nxtAfterPairCnt);
+        }
+    }
+    cellCount[x][y][MCtype]--;
+}
+
+void RoutingGraph::add_cell(int x, int y, int cellIndex) {
+    //Add into placement
+    placement[x][y].insert(cellIndex);
+    //Add demand into graph
+    add_cell_demand_into_graph(x, y, cellInstances[cellIndex].masterCell);
+}
+
+void RoutingGraph::del_cell(int cellIndex) {
+    Cell& cell = cellInstances[cellIndex];
+    int x = cell.x;
+    int y = cell.y;
+    //Del from placement
+    placement[x][y].erase(cellIndex);
+    //Del demand from graph
+    del_cell_demand_from_graph(x, y, cellInstances[cellIndex].masterCell);
 }
 
 void RoutingGraph::add_net_demand_into_graph(int x, int y, int z, int netIndex) {
