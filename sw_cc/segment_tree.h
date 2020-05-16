b024fd60b416ea84f4bc690eaa0025e205d8a289
TakaneP <40864488+TakaneP@users.noreply.github.com>
2020-05-17 00:31:25 +0800

    Create segment_tree.h

---
+++ sw_cc/segment_tree.h
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#ifndef SEGMENTTREE
+#define SEGMENTTREE
+
+class RoutingGraph;
+
+struct SegmentTreeNode{
+    int minValue;
+};
+
+class SegmentTree{
+public:
+    SegmentTree() = delete;
+    SegmentTree(RoutingGraph& main): graph(main) {}
+    RoutingGraph& graph;
+    std::vector<std::vector<std::vector<SegmentTreeNode>>> node; //layer, n = |row| or |column|, 2*2^ceil(logn)
+    void build_ini(void);
+    void build(int treeNodeIndex, int lowerBound, int upperBound, int layer, int rowOrColIndex);
+    void pushup(int treeNodeIndex, int layer, int rowOrColIndex);
+    int get_remaining_supply(int startIndex, int endIndex, int layer, int rowOrColIndex);
+    int query(int treeNodeIndex, int lowerBound, int upperBound, int startIndex, int endIndex, int layer, int rowOrColIndex);
+};
+
+
+
+#endif
